2fa3a3de56e2085ac02c2d2fa197cb7c89432d6f
Елена <test@email.ru>
2024-11-18 18:54:49 +0500

    4pk1 lessend nullexception (load customers)

--- Pizza/MainWindow.xaml.cs
+++ Pizza/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using Pizza.Models;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,9 +17,11 @@ namespace Pizza
     /// </summary>
     public partial class MainWindow : Window
     {
+        PizzaDbkozlovtsevContext cont = new PizzaDbkozlovtsevContext();
         public MainWindow()
         {
             InitializeComponent();
+            MessageBox.Show(cont.Database.CanConnect().ToString());
         }
     }
 }
